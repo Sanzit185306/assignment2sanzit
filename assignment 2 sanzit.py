7204616e99ab4e9a9e1a8be5fd3e062668f4676c
Sanzit185306 <sambosanzit@gmail.com>
2024-12-04 23:32:44 +0100

    Add files via upload

---
+++ assignment 2 sanzit.py
@@ -0,0 +1,10 @@
+number = [21,22,23,24,25]
+school_list = ['shoe' ,
+               'pen' ,
+               'clothes' ,
+               'confectionaries' ,
+               'skin care' ,
+                         ]
+print(school_list)
+school_list.reverse()
+print(school_list) 
